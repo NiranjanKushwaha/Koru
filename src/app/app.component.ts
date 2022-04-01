86cbd712c1f1c7a9fc4a348c200b4e9bba7557d8
Niranjan Kumar <niranjan.kushwaha@mediassist.in>
2022-04-02 01:05:26 +0530

    new UI created and tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,87 @@
-import { Component, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {
+  Component,
+  ViewChild,
+  OnInit,
+  ViewChildren,
+  QueryList,
+} from '@angular/core';
+import { map, Observable } from 'rxjs';
 import { ContainerModalComponent } from './Components/container-modal/container-modal.component';
+import { DataShareService } from './services/data-share.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor() {}
-  @ViewChild('modal') private modal: ContainerModalComponent;
+export class AppComponent implements OnInit {
+  constructor(
+    private http: HttpClient,
+    private _dataShareService: DataShareService
+  ) {}
 
-  openModal() {
-    this.modal.open();
+  @ViewChildren('inps') inps: QueryList<any>;
+  isAllSelected: boolean = false;
+  selectedCheckBoxes: any = [];
+  allData: any = [];
+  allDataDeepCopy: any = [];
+  getAllData(): Observable<any[]> {
+    return this.http
+      .get<any>('../../../assets/data.json')
+      .pipe(map((response) => response));
+  }
+  ngOnInit(): void {
+    this.getAllData().subscribe((res: any) => {
+      this.allData = res.data;
+      this.allDataDeepCopy = res.data;
+    });
+  }
+
+  handleAllCheckboxes(data: any) {
+    console.log(data.target.checked);
+    this.isAllSelected = data.target.checked;
+    this.selectedCheckBoxes = [];
+    if (this.isAllSelected) {
+      this.inps.map((inputbox: any) => {
+        inputbox.nativeElement.checked = true;
+        this.selectedCheckBoxes.push({
+          id: parseInt(inputbox.nativeElement.id),
+        });
+      });
+    } else {
+      this.inps.map((inputbox, id) => {
+        inputbox.nativeElement.checked = false;
+      });
+    }
+
+    console.log('selected checkboxes: ', this.selectedCheckBoxes);
+  }
+
+  handleSingleCheckbox(event: any, checkboxId: number) {
+    const found = this.selectedCheckBoxes.some(
+      (checkbox: any) => checkbox.id === checkboxId
+    );
+    if (!found && event.target.checked) {
+      this.selectedCheckBoxes.push({
+        id: checkboxId,
+      });
+    } else if (found && !event.target.checked) {
+      this.selectedCheckBoxes = this.selectedCheckBoxes.filter(
+        (checkbox: any) => checkbox.id !== checkboxId
+      );
+    }
+    this.selectedCheckBoxes.sort((a: any, b: any) => a.id - b.id);
+    console.log('selectionList: ', this.selectedCheckBoxes);
+  }
+
+  handleDelete() {
+    this.allData = this.allDataDeepCopy.filter((item: any) => {
+      this.selectedCheckBoxes.forEach((checkbox: any) => {
+        if (item.id !== checkbox.id) {
+          return item;
+        }
+      });
+    });
   }
 }
